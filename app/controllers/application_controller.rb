938249c513750b5dcc68195685bcf0a9d547a9d8
Anton Kosolapov <anvkos@ya.ru>
2019-09-08 19:10:04 +0700

    staff can sign in and reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    self.send("#{resource.class.to_s.downcase.pluralize}_root_path")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'index#index'
   namespace :staffs do
+    devise_for :staffs, path: '', controllers: {
+        sessions: 'staffs/sessions'
+    }
     get '/', to: 'index#index', as: :root
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
