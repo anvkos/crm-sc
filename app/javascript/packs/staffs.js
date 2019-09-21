ee4985c9f067b2775b9903a662f5e39f357f6ff7
Anton Kosolapov <anvkos@ya.ru>
2019-09-21 12:47:09 +0700

    setup Quasar

--- app/javascript/packs/staffs.js
+++ app/javascript/packs/staffs.js
@@ -1,8 +1,37 @@
 /* eslint no-console: 0 */
-
 import Vue from 'vue'
-import '../stylesheets/staff'
+import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import 'quasar/dist/quasar.ie.polyfills'
+import '../app_staff/assets/styles/quasar.styl'
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QBtn
+} from 'quasar'
 import App from '../AppStaff.vue'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QBtn
+  },
+  directives: {
+  },
+  iconSet: iconSet,
+  plugins: {
+  }
+})
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Vue({
