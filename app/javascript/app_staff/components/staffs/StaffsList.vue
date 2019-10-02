dac750d1a942c537c7847ef0b4040df1189a0374
Anton Kosolapov <anvkos@ya.ru>
2019-10-02 15:51:41 +0700

    open modal StaffEdit by click on staff edit button #front

--- app/javascript/app_staff/components/staffs/StaffsList.vue
+++ app/javascript/app_staff/components/staffs/StaffsList.vue
@@ -7,14 +7,18 @@
       row-key="id"
       :pagination.sync="pagination"
     )
+      template(v-slot:body-cell-actions="props")
+        QTd(:props="props")
+          QBtn(label="edit" flat color="green" size="sm" :to="{ name: 'staffs.show', params: { id: props.value } }")
 </template>
 
 <script>
-import { QTable } from 'quasar';
+import { QTable, QTd } from 'quasar';
 
 export default {
   components: {
     QTable,
+    QTd,
   },
 
   props: {
@@ -30,6 +34,7 @@ export default {
         { name: 'id', label: '#',field: 'id', align: 'left', sortable: true },
         { name: 'fullname', label: 'Full name',field: 'fullname', sortable: true },
         { name: 'email', label: 'Email',field: 'email', sortable: true },
+        { name: 'actions', label: 'Actions', field: 'id' },
       ],
       pagination: {
         rowsPerPage: 20,
